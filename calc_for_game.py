f11d98a72a5db1466a0f951baa9fb202e5dba2c9
skmake <matboe0@gmail.com>
2025-01-09 09:51:34 +0100

    Create calc_for_game.py
    Cas level calc

---
+++ calc_for_game.py
@@ -0,0 +1,13 @@
+
+
+def skibidi_calc(first_num:float,operator:str,second_num:float):
+    if operator == '+':
+        return first_num + second_num
+    elif operator == '-':
+        return first_num - second_num
+    elif operator == '*':
+        return first_num * second_num
+    elif operator == '/':
+        return first_num / second_num
+    else:
+        return 'Invalid operator, do it again skibidi pap pap'
